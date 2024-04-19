d0fcd16f93953a455e922f1f274970a20fcd55cb
OrionLi <orionli@qq.com>
2024-04-19 19:13:59 +0800

    Initial commit

---
+++ trace-begin/src/main/java/io/github/orionli/tracebegin/TraceBeginApplication.java
@@ -0,0 +1,13 @@
+package io.github.orionli.tracebegin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraceBeginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraceBeginApplication.class, args);
+    }
+
+}
