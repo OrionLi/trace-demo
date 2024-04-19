6451dc1919fa34b9371afe0f8b63de3b2e214289
OrionLi <orionli@qq.com>
2024-04-19 19:15:38 +0800

    feat: 添加日志配置文件和启动类注解

--- trace-begin/src/main/java/io/github/orionli/tracebegin/TraceBeginApplication.java
+++ trace-begin/src/main/java/io/github/orionli/tracebegin/TraceBeginApplication.java
@@ -3,6 +3,12 @@ package io.github.orionli.tracebegin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ *
+ * @author OrionLi
+ * @date 2024/04/19
+ */
 @SpringBootApplication
 public class TraceBeginApplication {
 
